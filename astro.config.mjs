087759d94f9f8314355233ff84358e9cd2403e84
binaryash <113206462+binaryash@users.noreply.github.com>
2025-10-12 16:30:45 +0530

    changed navbar to dock and changed image to circle

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,9 +24,11 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config"; // Make sure this path is correct for your project
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), react()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
